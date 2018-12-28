db03e542640093feecaeb7aab856a83257bea9c3
lisawilliams <lisa@placeblogger.com>
2018-12-27 21:40:59 -0500

    Added `pen` folder to repository for new lesson sample code.

--- 01-intro/start/js/main.js
+++ 01-intro/start/js/main.js
@@ -1,23 +1,14 @@
 $(document).ready(function(){
 
-	
+// Init ScrollMagic
+var controller = new ScrollMagic.Controller();
+
+// build a scene
+var ourScene = new ScrollMagic.Scene({
+	triggerElement: '#project01'
+})
+
+.setClassToggle('#project01', 'fade-in') // add class to project01
+.addTo(controller);
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
